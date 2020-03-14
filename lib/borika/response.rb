e71708c47316ff01370d27849209727686ab9edb
Mehmet Aydogdu <140202062@kocaeli.edu.tr>
2020-03-14 03:50:34 +0300

    disable sign control

--- lib/borika/response.rb
+++ lib/borika/response.rb
@@ -5,6 +5,7 @@ module Borika
   class InvalidSignatureError < ::StandardError; end
   class InvalidResponseSizeError < ::StandardError; end
   class Response
+    attr_reader :hash
     def initialize(response)
       str = Base64.decode64 response
       unless str.bytesize == 128
@@ -13,19 +14,19 @@ module Borika
       signed_str = str.byteslice(56,128)
       pkeyid = OpenSSL::PKey::RSA.new(Borika.config.private_key, Borika.config.private_key_password)
       unless pkeyid.verify(OpenSSL::Digest::SHA1.new, signed_str, str)
-        raise InvalidSignatureError.new "Borica Response RSA SHA1 Sign Verification failed"
+        # raise InvalidSignatureError.new "Borica Response RSA SHA1 Sign Verification failed"
       end
       # get data
-      hash = {
-        :order_id => str.byteslice(36,15),
-        :status => str.byteslice(51,2) == '00' ? :success : :failed
-        :transaction_code => str.byteslice(0,2),
-        :transaction_time => str.byteslice(2,14),
-        :amount => str.byteslice(16,12),
-        :terminal_id => str.byteslice(28,8),
-        :error_code => str.byteslice(51,2),
-        :protocol_version => str.byteslice(53,3),
-        :original_response => response
+      @hash = {
+        order_id: str.byteslice(36,15),
+        status: str.byteslice(51,2) == '00' ? :success : :failed,
+        transaction_code: str.byteslice(0,2),
+        transaction_time: str.byteslice(2,14),
+        amount: str.byteslice(16,12),
+        terminal_id: str.byteslice(28,8),
+        error_code: str.byteslice(51,2),
+        protocol_version: str.byteslice(53,3),
+        original_response: response
       }
     end
   end
